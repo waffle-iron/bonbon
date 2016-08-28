f77f64abe8ca8fe7fe414e24265231dc217cab40
Stefan Johnson <sj@scrimpycat.io>
2016-08-28 15:49:28 +1000

    Add dependencies to PLT

--- mix.exs
+++ mix.exs
@@ -10,8 +10,9 @@ defmodule Bonbon.Mixfile do
             compilers: [:phoenix, :gettext] ++ Mix.compilers,
             build_embedded: Mix.env == :prod,
             start_permanent: Mix.env == :prod,
-            aliases: aliases(),
-            deps: deps()
+            aliases: aliases,
+            deps: deps,
+            dialyzer: [plt_add_deps: :transitive]
         ]
     end
 
