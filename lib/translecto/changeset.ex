613212f7386f68aeaa1e03d5242456e7850a6f5e
Stefan Johnson <sj@scrimpycat.io>
2016-08-29 22:08:35 +1000

    Added docs and spec to changeset

--- lib/translecto/changeset.ex
+++ lib/translecto/changeset.ex
@@ -1,6 +1,15 @@
 defmodule Translecto.Changeset do
     import Ecto.Changeset
+    @moduledoc """
+      Convenient changesets to perform validation and casting of translation fields.
+    """
 
+    @type changeset :: Ecto.Schema.t | Ecto.Changeset.t | { Ecto.Changeset.data, Ecto.Changeset.types }
+
+    @doc """
+      Validate and cast a translation model.
+    """
+    @spec translation_changeset(changeset, %{}, keyword()) :: changeset
     def translation_changeset(struct, params, opts \\ []) do
         pkey = String.to_atom(Ecto.get_meta(struct, :source) <> "_pkey")
 
@@ -11,6 +20,10 @@ defmodule Translecto.Changeset do
         |> unique_constraint(pkey, name: pkey) #todo: don't handle when primary_key is set to false
     end
 
+    @doc """
+      Validate and cast a translatable field.
+    """
+    @spec translatable_changeset(changeset, %{}, [String.t | atom], keyword()) :: changeset
     def translatable_changeset(struct, params, allowed, opts \\ []) do
         struct
         |> cast(params, allowed)
