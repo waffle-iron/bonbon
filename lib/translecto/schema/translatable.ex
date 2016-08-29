40867ad200d452bac6efa16b126b65989dc344eb
Stefan Johnson <sj@scrimpycat.io>
2016-08-29 22:31:44 +1000

    Added docs and specs for translatable schema

--- lib/translecto/schema/translatable.ex
+++ lib/translecto/schema/translatable.ex
@@ -1,5 +1,11 @@
 defmodule Translecto.Schema.Translatable do
     import Ecto.Schema
+    @moduledoc """
+      Reference a translatable field in the schema.
+
+      This module coincides with the migration function `Translecto.Migration.translate/2`.
+      To correctly use this module a schema should call `use Translecto.Schema.Translatable`.
+    """
 
     defmacro __using__(options) do
         quote do
@@ -22,6 +28,29 @@ defmodule Translecto.Schema.Translatable do
         end
     end
 
+    @doc """
+      Expose a field as being translatable to the schema.
+
+      The name of the field specified should coincide with a migration table field that was made
+      using `Translecto.Migration.translate/2`.
+
+      The queryable should be the translation module (schema) that represents the translation table.
+
+        defmodule Ingredient do
+            use Translecto.Schema.Translatable
+
+            schema "ingredients" do
+                translatable :name, Ingredient.Translation
+            end
+
+            def changeset(struct, params \\\\ %{}) do
+                struct
+                |> translatable_changeset(params, [:name])
+                |> validate_required([:name])
+            end
+        end
+    """
+    @spec translatable(atom, module(), keyword()) :: Macro.t
     defmacro translatable(name, queryable, opts \\ []) do
         Module.put_attribute(__CALLER__.module, :translecto_translate, [{ name, queryable }|(Module.get_attribute(__CALLER__.module, :translecto_translate) || [])])
 
