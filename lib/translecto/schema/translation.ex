40366048ea08e5c97e91d5a7b544c66810f84e70
Stefan Johnson <sj@scrimpycat.io>
2016-08-29 22:27:07 +1000

    Added docs and specs for translation schema

--- lib/translecto/schema/translation.ex
+++ lib/translecto/schema/translation.ex
@@ -1,5 +1,13 @@
 defmodule Translecto.Schema.Translation do
     import Ecto.Schema
+    @moduledoc """
+      Sets up a translation schema.
+
+      This module coincides with the migration function `Translecto.Migration.translation/1`.
+      To correctly use this module a schema should call `use Translecto.Schema.Translation`,
+      overriding the default primary_key behaviour if needed and then adding the `translation/0`
+      macro to the schema.
+    """
 
     defmacro __using__(options) do
         pkey = if Keyword.get(options, :primary_key, true) do
@@ -14,6 +22,25 @@ defmodule Translecto.Schema.Translation do
         end
     end
 
+    @doc """
+      Setup the schema as a translation.
+
+        defmodule Ingredient.Translation do
+            use Translecto.Schema.Translation
+
+            schema "ingredient_translations" do
+                translation
+                field :term, :string
+            end
+
+            def changeset(struct, params \\\\ %{}) do
+                struct
+                |> translation_changeset(params)
+                |> cast(params, [:term])
+                |> validate_required([:term])
+            end
+        end
+    """
     defmacro translation(opts \\ []) do
         quote do
             belongs_to :locale, Bonbon.Locale
