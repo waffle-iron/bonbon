e076a0f6871b3a5a45dc0d2cff3bb11b6b4168b8
Stefan Johnson <sj@scrimpycat.io>
2016-09-03 19:00:52 +1000

    Merged food related migrations

--- priv/repo/migrations/20160829125439_create_item_food.exs
+++ priv/repo/migrations/20160829125439_create_item_food.exs
@@ -3,6 +3,20 @@ defmodule Bonbon.Repo.Migrations.CreateItem.Food do
     import Translecto.Migration
 
     def change do
+        create table(:food_content_translations, primary_key: false) do
+            translation
+
+            add :name, :string,
+                null: false#,
+                # comment: "The localised name of the food"
+
+            add :description, :string,
+                null: false#,
+                # comment: "The localised description of the food"
+
+            timestamps
+        end
+
         create table(:foods) do
             #todo: type field?
             translate :content,
@@ -31,5 +45,40 @@ defmodule Bonbon.Repo.Migrations.CreateItem.Food do
         end
 
         create index(:foods, [:content], unique: true)
+
+        create table(:food_ingredient_list) do
+            add :food_id, references(:foods),
+                null: false
+
+            add :ingredient_id, references(:ingredients),
+                null: false
+
+            add :addon, :boolean,
+                default: false,
+                null: false
+
+            add :price, :decimal,
+                null: false
+
+            add :currency, :char, #ISO 4217
+                size: 3,
+                null: false
+
+            timestamps
+        end
+
+        create index(:food_ingredient_list, [:food_id, :ingredient_id], unique: true)
+
+        create table(:food_diet_list) do
+            add :food_id, references(:foods),
+                null: false
+
+            add :diet_id, references(:diets),
+                null: false
+
+            timestamps
+        end
+
+        create index(:food_diet_list, [:food_id, :diet_id], unique: true)
     end
 end
