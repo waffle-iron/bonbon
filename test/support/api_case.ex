1f76c51b945b27226565d08419a52883782adad0
Stefan Johnson <sj@scrimpycat.io>
2016-09-23 04:54:32 +1000

    Added docs to convenient query macros

--- test/support/api_case.ex
+++ test/support/api_case.ex
@@ -41,6 +41,19 @@ defmodule Bonbon.APICase do
         { :ok, conn: conn }
     end
 
+    @doc """
+      Submit a GraphQL query and retrieve the result.
+
+      This macro simplifies running GraphQL calls by handling the submission, response,
+      and converting the JSON result to an Elixir Map.
+
+      The `conn` argument is the current Plug connection to be used to send the request.
+
+      The `query` argument is the GraphQL query itself.
+
+      The `code` argument is the status code returned. See `[Plug.Conn.Status](https://hexdocs.pm/plug/Plug.Conn.Status.html)`
+    """
+    @spec run(Plug.Conn.t, String.t, integer | atom) :: Macro.t
     defmacro run(conn, query, code \\ :ok) do
         quote do
             Poison.decode!(response(post(unquote(conn), "/", unquote(query)), unquote(code)))
@@ -63,23 +76,55 @@ defmodule Bonbon.APICase do
     #todo: need to add support for more elaborate queries
     defp build_query(root, fields, args), do: "{ #{to_root(root)}#{to_args(args)}#{to_fields(fields)} }"
 
+    @doc """
+      Build and run a GraphQL query.
+
+      This macro simplifies constructing GraphQL calls. It then passes them to `run/3`.
+
+      The `conn` argument is the current Plug connection to be used to send the request.
+
+      The `root` argument is the root GraphQL query type.
+
+      The `fields` argument is the GraphQL subfields.
+
+      The `args` argument are the GraphQL query arguments.
+    """
+    @spec query(Plug.Conn.t, atom, [atom], keyword()) :: Macro.t
     defmacro query(conn, root, fields, args \\ []) do
         quote do
             run(unquote(conn), unquote(build_query(root, fields, args)))
         end
     end
 
+    @doc """
+      Build and run a GraphQL query, and retrieve the root data.
+
+      This macro simplifies constructing GraphQL calls, and retrieving the root data. For
+      more information see: `query/4`
+    """
+    @spec query_data(Plug.Conn.t, atom, [atom], keyword()) :: Macro.t
     defmacro query_data(conn, root, fields, args \\ []) do
         quote do
             query(unquote(conn), unquote(root), unquote(fields), unquote(args))["data"][unquote(to_root(root))]
         end
     end
 
+    @doc """
+      Get the custom portion of the error message.
+    """
+    @spec get_message(String.t) :: String.t
     def get_message(message) do
         [_,message] = String.split(message, ":", parts: 2)
         String.trim(message)
     end
 
+    @doc """
+      Build and run a GraphQL query, and retrieve the custom portion of the root error message.
+
+      This macro simplifies constructing GraphQL calls, and retrieving the root error message.
+      For more information see: `query/4`
+    """
+    @spec query_error(Plug.Conn.t, atom, [atom], keyword()) :: Macro.t
     defmacro query_error(conn, root, fields, args \\ []) do
         quote do
             get_message(List.first(query(unquote(conn), unquote(root), unquote(fields), unquote(args))["errors"])["message"])
