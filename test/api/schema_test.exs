11e3e3f4d5e31d14a811cc9fb6b7154b1cbec91d
Stefan Johnson <sj@scrimpycat.io>
2016-09-23 14:30:47 +1000

    Finish test coverage of ingredients

--- test/api/schema_test.exs
+++ test/api/schema_test.exs
@@ -34,21 +34,90 @@ defmodule Bonbon.API.SchemaTest do
 
     @tag locale: "en"
     test "get ingredient without id", %{ conn: conn, db: db } do
-        assert "1 required argument (`id') not provided" == query_error(conn, :ingredient, [:id, :name, :type])
+        assert "1 required argument (`id') not provided" == query_error(conn, :ingredient, [:id, :name, :type]) #todo: possibly just check that an error was returned
     end
 
+    #ingredient(id:)
     test_localisable_query("get ingredient with invalid id", nil, :ingredient, [:id, :name, :type], id: 0)
 
     test_localisable_query("get ingredient with id", &(&2[&1].ingredient.lemon), :ingredient, [:id, :name, :type], id: &(&1.en.ingredient.lemon["id"]))
 
+    @tag locale: "en"
+    test "get ingredient with non-integer id", %{ conn: conn, db: db } do
+        assert _ = query_error(conn, :ingredient, [:id, :name, :type], id: "test") #todo: change to custom formatted message
+    end
+
+    #ingredients
     test_localisable_query("list all ingredients", &(Map.values(&2[&1].ingredient)), :ingredients, [:id, :name, :type])
 
+    test_localisable_query("list first ingredient", &([&2[&1].ingredient.apple]), :ingredients, [:id, :name, :type], limit: 1)
+
+    test_localisable_query("list second ingredient", &([&2[&1].ingredient.lemon]), :ingredients, [:id, :name, :type], limit: 1, offset: 1)
+
+    @tag locale: "en"
+    test "list ingredients with negative limit", %{ conn: conn, db: db } do
+        assert "LIMIT must not be negative" == query_error(conn, :ingredients, [:id, :name, :type], limit: -1) #todo: change to custom formatted message
+    end
+
+    @tag locale: "en"
+    test "list ingredients with negative offset", %{ conn: conn, db: db } do
+        assert "OFFSET must not be negative" == query_error(conn, :ingredients, [:id, :name, :type], offset: -1) #todo: change to custom formatted message
+    end
+
+    @tag locale: "en"
+    test "list ingredients with non-integer limit", %{ conn: conn, db: db } do
+        assert "1 badly formed argument (`limit') provided" == query_error(conn, :ingredients, [:id, :name, :type], limit: "test") #todo: possibly just check that an error was returned
+    end
+
+    @tag locale: "en"
+    test "list ingredients with non-integer offset", %{ conn: conn, db: db } do
+        assert "1 badly formed argument (`offset') provided" == query_error(conn, :ingredients, [:id, :name, :type], offset: "test") #todo: possibly just check that an error was returned
+    end
+
+    #ingredients(find:)
     test_localisable_query("find 'ap' in ingredients", fn
         :en, db -> [db.en.ingredient.apple]
         :fr, _ -> []
     end, :ingredients, [:id, :name, :type], find: "ap")
 
+    test_localisable_query("find 'pomme' in ingredients", fn
+        :en, _ -> []
+        :fr, db -> [db.fr.ingredient.apple]
+    end, :ingredients, [:id, :name, :type], find: "pomme")
+
     test_localisable_query("find 'fr' in ingredients", &(Map.values(&2[&1].ingredient)), :ingredients, [:id, :name, :type], find: "fr")
 
-    test_localisable_query("find 'zz' in ingredients", fn _, _ -> [] end, :ingredients, [:id, :name, :type], find: "zz")
+    test_localisable_query("find 'zz' in ingredients", [], :ingredients, [:id, :name, :type], find: "zz")
+
+    #ingredients(name:)
+    test_localisable_query("find name 'ap' in ingredients", fn
+        :en, db -> [db.en.ingredient.apple]
+        :fr, _ -> []
+    end, :ingredients, [:id, :name, :type], name: "ap")
+
+    test_localisable_query("find name 'pomme' in ingredients", fn
+        :en, _ -> []
+        :fr, db -> [db.fr.ingredient.apple]
+    end, :ingredients, [:id, :name, :type], name: "pomme")
+
+    test_localisable_query("find name 'fr' in ingredients", [], :ingredients, [:id, :name, :type], name: "fr")
+
+    test_localisable_query("find name 'zz' in ingredients", [], :ingredients, [:id, :name, :type], name: "zz")
+
+    #ingredients(type:)
+    test_localisable_query("find type 'ap' in ingredients", [], :ingredients, [:id, :name, :type], type: "ap")
+
+    test_localisable_query("find type 'pomme' in ingredients", [], :ingredients, [:id, :name, :type], type: "pomme")
+
+    test_localisable_query("find type 'fr' in ingredients", &(Map.values(&2[&1].ingredient)), :ingredients, [:id, :name, :type], type: "fr")
+
+    test_localisable_query("find type 'zz' in ingredients", [], :ingredients, [:id, :name, :type], type: "zz")
+
+    #ingredients(name:, type:, find:)
+    test_localisable_query("find 'ci' name 'ap', type 'fr' in ingredients", [], :ingredients, [:id, :name, :type], find: "ci", name: "ap", type: "fr")
+
+    test_localisable_query("find 'ci' name 'c', type 'fr' in ingredients", fn
+        :en, _ -> []
+        :fr, db -> [db.fr.ingredient.lemon]
+    end, :ingredients, [:id, :name, :type], find: "ci", name: "c", type: "fr")
 end
