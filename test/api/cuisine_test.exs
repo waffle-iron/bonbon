c783df458d8a9627d07c5fc48a736c69339ac12a
Stefan Johnson <sj@scrimpycat.io>
2016-09-25 09:16:50 +1000

    Complete test coverage of cuisine query

--- test/api/cuisine_test.exs
+++ test/api/cuisine_test.exs
@@ -65,4 +65,114 @@ defmodule Bonbon.API.CuisineTest do
     test "get cuisine with non-integer id", %{ conn: conn } do
         assert _ = query_error(conn, id: "test") #todo: change to custom formatted message
     end
+
+    #regions
+    @root :cuisines
+
+    test_localisable_query("list all cuisines", &([&2[&1].cuisine.pasta, &2[&1].cuisine.lamington]))
+
+    test_localisable_query("list first cuisine", &([&2[&1].cuisine.pasta]), limit: 1)
+
+    test_localisable_query("list second cuisine", &([&2[&1].cuisine.lamington]), limit: 1, offset: 1)
+
+    @tag locale: "en"
+    test "list cuisines with negative limit", %{ conn: conn } do
+        assert "LIMIT must not be negative" == query_error(conn, limit: -1) #todo: change to custom formatted message
+    end
+
+    @tag locale: "en"
+    test "list cuisines with negative offset", %{ conn: conn } do
+        assert "OFFSET must not be negative" == query_error(conn, offset: -1) #todo: change to custom formatted message
+    end
+
+    @tag locale: "en"
+    test "list cuisines with non-integer limit", %{ conn: conn } do
+        assert "1 badly formed argument (`limit') provided" == query_error(conn, limit: "test") #todo: possibly just check that an error was returned
+    end
+
+    @tag locale: "en"
+    test "list cuisines with non-integer offset", %{ conn: conn } do
+        assert "1 badly formed argument (`offset') provided" == query_error(conn, offset: "test") #todo: possibly just check that an error was returned
+    end
+
+    #cuisines(find:)
+    test_localisable_query("find 'océa' in cuisines", fn
+        :en, _ -> []
+        :fr, db -> [db.fr.cuisine.lamington]
+    end, find: "océa")
+
+    test_localisable_query("find 'lam' in cuisines", &([&2[&1].cuisine.lamington]), find: "lam")
+
+    test_localisable_query("find 'au' in cuisines", &([&2[&1].cuisine.lamington]), find: "au")
+
+    test_localisable_query("find 'br' in cuisines", &([&2[&1].cuisine.lamington]), find: "br")
+
+    test_localisable_query("find 'zz' in cuisines", [], find: "zz")
+
+    #cuisines(region: { continent: })
+    test_localisable_query("find continent 'océa' in cuisines", fn
+        :en, _ -> []
+        :fr, db -> [db.fr.cuisine.lamington]
+    end, region: [continent: "océa"])
+
+    test_localisable_query("find continent 'lam' in cuisines", [], region: [continent: "lam"])
+
+    test_localisable_query("find continent 'e' in cuisines", &([&2[&1].cuisine.pasta]), region: [continent: "e"])
+
+    test_localisable_query("find continent 'au' in cuisines", [], region: [continent: "au"])
+
+    test_localisable_query("find continent 'zz' in cuisines", [], region: [continent: "zz"])
+
+    #cuisines(region: { subregion: })
+    test_localisable_query("find subregion 'océa' in cuisines", [], region: [subregion: "océa"])
+
+    test_localisable_query("find subregion 'lam' in cuisines", [], region: [subregion: "lam"])
+
+    test_localisable_query("find subregion 'e' in cuisines", [], region: [subregion: "e"])
+
+    test_localisable_query("find subregion 'au' in cuisines", &([&2[&1].cuisine.lamington]), region: [subregion: "au"])
+
+    test_localisable_query("find subregion 'zz' in cuisines", [], region: [subregion: "zz"])
+
+    # #cuisines(region: { country: })
+    test_localisable_query("find country 'océa' in cuisines", [], region: [country: "océa"])
+
+    test_localisable_query("find country 'lam' in cuisines", [], region: [country: "lam"])
+
+    test_localisable_query("find country 'e' in cuisines", [], region: [country: "e"])
+
+    test_localisable_query("find country 'au' in cuisines", &([&2[&1].cuisine.lamington]), region: [country: "au"])
+
+    test_localisable_query("find country 'zz' in cuisines", [], region: [country: "zz"])
+
+    #cuisines(region: { province: })
+    test_localisable_query("find province 'br' in cuisines",  &([&2[&1].cuisine.lamington]), region: [province: "br"])
+
+    test_localisable_query("find province 'lam' in cuisines", [], region: [province: "lam"])
+
+    test_localisable_query("find province 'e' in cuisines", [], region: [province: "e"])
+
+    test_localisable_query("find province 'au' in cuisines", [], region: [province: "au"])
+
+    test_localisable_query("find province 'zz' in cuisines", [], region: [province: "zz"])
+
+    #cuisines(region: { id: })
+    test_localisable_query("find region id in cuisines",  &([&2[&1].cuisine.lamington]), region: [id: &(&1.en.cuisine.lamington["region"]["id"])])
+
+    test_localisable_query("find invalid region id in cuisines", [], region: [id: 0])
+
+    @tag locale: "en"
+    test "find non-integer region id in cuisines", %{ conn: conn } do
+        assert _ = query_error(conn, region: [id: "test"]) #todo: change to custom formatted message
+    end
+
+    #cuisines(region: { id:, continent:, subregion:, country:, province: }, find:)
+    test_localisable_query("find 'p' continent 'oc', subregion 'au', country 'au', province 'br' with id in cuisines", [], find: "p", region: [id: &(&1.en.cuisine.lamington["region"]["id"]), continent: "oc", subregion: "au", country: "au", province: "br"])
+
+    test_localisable_query("find 'l' continent 'oc', subregion 'au', country 'au', province 'br' with id in cuisines", &([&2[&1].cuisine.lamington]), find: "l", region: [id: &(&1.en.cuisine.lamington["region"]["id"]), continent: "oc", subregion: "au", country: "au", province: "br"])
+
+    test_localisable_query("find 'l' continent 'océ', subregion 'au', country 'au', province 'br' with id in cuisines", fn
+        :en, _ -> []
+        :fr, db -> [db.fr.cuisine.lamington]
+    end, find: "l", region: [id: &(&1.en.cuisine.lamington["region"]["id"]), continent: "océ", subregion: "au", country: "au", province: "br"])
 end
