8a27bdbc0826ed12c89c33786be6a523d6265fc1
Stefan Johnson <sj@scrimpycat.io>
2016-09-29 09:13:43 +1000

    Converted to use Absinthe v1.2

--- test/api/diet_test.exs
+++ test/api/diet_test.exs
@@ -36,7 +36,7 @@ defmodule Bonbon.API.DietTest do
 
     @tag locale: "en"
     test "get diet without id", %{ conn: conn } do
-        assert "1 required argument (`id') not provided" == query_error(conn) #todo: possibly just check that an error was returned
+        assert nil != query_error(conn, @root, @fields, [], :bad_request) #todo: possibly just check that an error was returned
     end
 
     #diet(id:)
@@ -70,12 +70,12 @@ defmodule Bonbon.API.DietTest do
 
     @tag locale: "en"
     test "list diets with non-integer limit", %{ conn: conn } do
-        assert "1 badly formed argument (`limit') provided" == query_error(conn, limit: "test") #todo: possibly just check that an error was returned
+        assert nil != query_error(conn, @root, @fields, [limit: "test"], :bad_request) #todo: possibly just check that an error was returned
     end
 
     @tag locale: "en"
     test "list diets with non-integer offset", %{ conn: conn } do
-        assert "1 badly formed argument (`offset') provided" == query_error(conn, offset: "test") #todo: possibly just check that an error was returned
+        assert nil != query_error(conn, @root, @fields, [offset: "test"], :bad_request) #todo: possibly just check that an error was returned
     end
 
     #diets(name:)
