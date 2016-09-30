7cfc3f447129f4fbe2f8bae16054309e5d4147a0
Stefan Johnson <sj@scrimpycat.io>
2016-09-30 17:43:05 +1000

    Cleanup and added another convenient insert variant

--- priv/repo/data_import/translation_data.exs
+++ priv/repo/data_import/translation_data.exs
@@ -1,14 +1,15 @@
 defmodule Bonbon.Repo.DataImport.TranslationData do
     def insert!(model, data) do
         for { { _label, translation }, group } <- Enum.with_index(data) do
-            insert!(model, translation, [], group + 1)
+            insert!(model, translation, group + 1)
         end
         |> List.flatten
         |> Enum.filter_map(&(&1 != nil), &(&1.translate_id))
         |> Enum.uniq
     end
 
-    def insert!(model, string, [_|language], group) when is_binary(string) do
+    def insert!(model, translation, group, language \\ [])
+    def insert!(model, string, group, [_|language]) when is_binary(string) do
         try do
             Bonbon.Model.Locale.to_locale_id(Enum.reverse(language) |> Enum.join("_"))
         else
@@ -18,9 +19,9 @@ defmodule Bonbon.Repo.DataImport.TranslationData do
             _ -> nil
         end
     end
-    def insert!(model, data, language, group) do
+    def insert!(model, data, group, language) do
         for { locale, translation } <- data do
-            insert!(model, translation, [to_string(locale)|language], group)
+            insert!(model, translation, group, [to_string(locale)|language])
         end
     end
 end
