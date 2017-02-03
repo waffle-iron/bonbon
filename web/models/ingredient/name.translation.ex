605e5335647709ea05de93bd9b6e139f8588c7b0
Stefan Johnson <sj@scrimpycat.io>
2017-02-03 14:16:28 +1100

    Added detailed docs for ingredient name translation model

--- web/models/ingredient/name.translation.ex
+++ web/models/ingredient/name.translation.ex
@@ -4,6 +4,19 @@ defmodule Bonbon.Model.Ingredient.Name.Translation do
     @moduledoc """
       A model representing the different ingredient names for the different
       translations.
+
+      ##Fields
+
+      ###:translate_id
+      Is the reference to the associated group of ingredient name entries. Is
+      an `integer`.
+
+      ###:locale_id
+      Is the reference to the specific translations for a given localisation.
+      Is an `integer`.
+
+      ###:term
+      Is the ingredient name. Is a `string`.
     """
 
     schema "ingredient_name_translations" do
@@ -14,6 +27,11 @@ defmodule Bonbon.Model.Ingredient.Name.Translation do
 
     @doc """
       Builds a changeset based on the `struct` and `params`.
+
+      Enforces:
+      * it's a translation
+      * `term` field is required
+      * formats the `term` field as lowercase
     """
     def changeset(struct, params \\ %{}) do
         struct
