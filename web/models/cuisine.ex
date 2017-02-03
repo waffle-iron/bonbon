23ae9080048668d143356940ca42953982c164e5
Stefan Johnson <sj@scrimpycat.io>
2017-02-03 15:28:13 +1100

    Added detailed docs for cuisine model

--- web/models/cuisine.ex
+++ web/models/cuisine.ex
@@ -3,6 +3,18 @@ defmodule Bonbon.Model.Cuisine do
     use Translecto.Schema.Translatable
     @moduledoc """
       A model representing the different cuisines.
+
+      ##Fields
+
+      ###:id
+      Is the unique reference to the cuisine entry. Is an `integer`.
+
+      ###:name
+      Is the name of the cuisine. Is a `translatable`.
+
+      ###:region_id
+      Is the reference to the region the cuisine belongs to. Is an
+      `integer` to `Bonbon.Model.Cuisine.Region`.
     """
 
     schema "cuisines" do
@@ -13,6 +25,13 @@ defmodule Bonbon.Model.Cuisine do
 
     @doc """
       Builds a changeset based on the `struct` and `params`.
+
+      Enforces:
+      * `name` field is translatable
+      * `name` field is required
+      * `region_id` field is required
+      * `region_id` field is associated with an entry in `Bonbon.Model.Cuisine.Region`
+      * `name` field is unique
     """
     def changeset(struct, params \\ %{}) do
         struct
