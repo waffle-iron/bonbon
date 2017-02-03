cc8122d3f15f191f84ba9b9ede2e48f41e5c5f45
Stefan Johnson <sj@scrimpycat.io>
2017-02-03 15:44:39 +1100

    Added detailed docs for region subregion translation model

--- web/models/cuisine/region/subregion.translation.ex
+++ web/models/cuisine/region/subregion.translation.ex
@@ -4,6 +4,19 @@ defmodule Bonbon.Model.Cuisine.Region.Subregion.Translation do
     @moduledoc """
       A model representing the different subregion names for the different
       translations.
+
+      ##Fields
+
+      ###:translate_id
+      Is the reference to the associated group of region subregion entries.
+      Is an `integer`.
+
+      ###:locale_id
+      Is the reference to the specific translations for a given localisation.
+      Is an `integer`.
+
+      ###:term
+      Is the region subregion. Is a `string`.
     """
 
     schema "cuisine_region_subregion_translations" do
@@ -14,6 +27,11 @@ defmodule Bonbon.Model.Cuisine.Region.Subregion.Translation do
 
     @doc """
       Builds a changeset based on the `struct` and `params`.
+
+      Enforces:
+      * it's a translation
+      * `term` field is required
+      * formats the `term` field as lowercase
     """
     def changeset(struct, params \\ %{}) do
         struct
