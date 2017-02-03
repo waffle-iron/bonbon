6e0588d189deb2203ae8858329017d0fe04e42b6
Stefan Johnson <sj@scrimpycat.io>
2017-02-03 15:34:49 +1100

    Added detailed docs for region model

--- web/models/cuisine/region.ex
+++ web/models/cuisine/region.ex
@@ -4,6 +4,23 @@ defmodule Bonbon.Model.Cuisine.Region do
     import Translecto.Changeset
     @moduledoc """
       A model representing the different cuisine regions.
+
+      ##Fields
+
+      ###:id
+      Is the unique reference to the region entry. Is an `integer`.
+
+      ###:continent
+      Is the continent of the region. Is a `translatable`.
+
+      ###:subregion
+      Is the subregion of the region. Is a `translatable`.
+
+      ###:country
+      Is the country of the region. Is a `translatable`.
+
+      ###:province
+      Is the province of the region. Is a `translatable`.
     """
 
     schema "cuisine_regions" do
@@ -16,6 +33,14 @@ defmodule Bonbon.Model.Cuisine.Region do
 
     @doc """
       Builds a changeset based on the `struct` and `params`.
+
+      Enforces:
+      * `continent` field is translatable
+      * `subregion` field is translatable
+      * `country` field is translatable
+      * `province` field is translatable
+      * `continent` field is required
+      * checks uniqueness of the given region fields
     """
     def changeset(struct, params \\ %{}) do
         struct
