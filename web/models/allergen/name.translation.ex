185c14b835feaf1c604b3197b7b50aa93e280473
Stefan Johnson <sj@scrimpycat.io>
2017-02-03 14:15:41 +1100

    Added detailed docs for allergen name translation model

--- web/models/allergen/name.translation.ex
+++ web/models/allergen/name.translation.ex
@@ -4,6 +4,19 @@ defmodule Bonbon.Model.Allergen.Name.Translation do
     @moduledoc """
       A model representing the different allergen names for the different
       translations.
+
+      ##Fields
+
+      ###:translate_id
+      Is the reference to the associated group of allergen name entries. Is
+      an `integer`.
+
+      ###:locale_id
+      Is the reference to the specific translations for a given localisation.
+      Is an `integer`.
+
+      ###:term
+      Is the allergen name. Is a `string`.
     """
 
     schema "allergen_name_translations" do
@@ -14,6 +27,11 @@ defmodule Bonbon.Model.Allergen.Name.Translation do
 
     @doc """
       Builds a changeset based on the `struct` and `params`.
+
+      Enforces:
+      * it's a translation
+      * `term` field is required
+      * formats the `term` field as lowercase
     """
     def changeset(struct, params \\ %{}) do
         struct
