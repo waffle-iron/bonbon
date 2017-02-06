1f701b9682121161b8623d5caf905d69cb3e5b4d
Stefan Johnson <sj@scrimpycat.io>
2017-02-06 21:34:45 +1100

    Config guardian

--- config/dev.exs
+++ config/dev.exs
@@ -30,3 +30,13 @@ config :bonbon, Bonbon.Repo,
     hostname: "localhost",
     pool_size: 10,
     types: Bonbon.PostgresTypes
+
+config :guardian, Guardian,
+    allowed_algos: ["HS512"],
+    verify_module: Guardian.JWT,
+    issuer: "Bonbon",
+    ttl: { 30, :days },
+    allowed_drift: 2000,
+    verify_issuer: true,
+    secret_key: "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.e30.6bK5p0FPG1KY68mstRXiUjWtti5EbPmDg0QxP702j3WTEcI16GXZAU0NlXMQFnyPsrDyqCv9p6KRqMg7LcswMg",
+    serializer: Bonbon.GuardianSerializer
--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -11,3 +11,13 @@ config :bonbon, Bonbon.Repo,
     adapter: Ecto.Adapters.Postgres,
     url: System.get_env("DATABASE_URL"),
     pool_size: 20
+
+config :guardian, Guardian,
+    allowed_algos: ["HS512"],
+    verify_module: Guardian.JWT,
+    issuer: "Bonbon",
+    ttl: { 30, :days },
+    allowed_drift: 2000,
+    verify_issuer: true,
+    secret_key: System.get_env("GUARDIAN_SECRET_KEY"),
+    serializer: Bonbon.GuardianSerializer
