4f52cf10fae0f0c11d1b877922366f012ebb63f8
Stefan Johnson <sj@scrimpycat.io>
2017-02-08 00:44:12 +1100

    Add hash formatter

--- web/models/changeset_helpers.ex
+++ web/models/changeset_helpers.ex
@@ -24,4 +24,22 @@ defmodule Bonbon.ChangesetHelpers do
             _ -> changeset
         end
     end
+
+    @doc """
+      Hash the given string field, and pass it into the field name followed by
+      `_hash`.
+    """
+    @spec format_hash(Ecto.Changeset.t, atom) :: Ecto.Changeset.t
+    def format_hash(changeset, field), do: format_hash(changeset, field, String.to_atom(to_string(field) <> "_hash"))
+
+    @doc """
+      Hash the given string field, and pass it into the hash_field.
+    """
+    @spec format_hash(Ecto.Changeset.t, atom, atom) :: Ecto.Changeset.t
+    def format_hash(changeset, field, hash_field) do
+        case changeset do
+            %Ecto.Changeset{ valid?: true, changes: %{ ^field => value } } when is_binary(value) -> Ecto.Changeset.put_change(changeset, hash_field, Comeonin.Bcrypt.hashpwsalt(value))
+            _ -> changeset
+        end
+    end
 end
