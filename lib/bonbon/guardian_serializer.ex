3fc05ea9e33c339ea27b0e232527e1b81b415599
Stefan Johnson <sj@scrimpycat.io>
2017-02-16 18:16:33 +1100

    Fix id match

--- lib/bonbon/guardian_serializer.ex
+++ lib/bonbon/guardian_serializer.ex
@@ -4,6 +4,6 @@ defmodule Bonbon.GuardianSerializer do
     def for_token(user = %Bonbon.Model.Account.User{}), do: { :ok, "User:#{user.id}" }
     def for_token(_), do: { :error, "Unknown resource type" }
 
-    def from_token(<<"User:", id>>), do: { :ok, Bonbon.Repo.get(Bonbon.Model.Account.User, id) }
+    def from_token("User:" <> id), do: { :ok, Bonbon.Repo.get(Bonbon.Model.Account.User, id) }
     def from_token(_), do: { :error, "Unknown resource type" }
 end
