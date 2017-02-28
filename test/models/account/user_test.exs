0a4636041da3259642bb10bd73e75a0010c80f80
Stefan Johnson <sj@scrimpycat.io>
2017-02-28 20:55:46 +1100

    Changed assertions to test registration_changeset

--- test/models/account/user_test.exs
+++ test/models/account/user_test.exs
@@ -12,89 +12,89 @@ defmodule Bonbon.Model.Account.UserTest do
     }
 
     test "empty" do
-        refute_change(%User{})
+        refute_change(%User{}, %{}, :registration_changeset)
     end
 
     test "only email" do
-        refute_change(%User{}, %{ email: @valid_model.email })
+        refute_change(%User{}, %{ email: @valid_model.email }, :registration_changeset)
     end
 
     test "only name" do
-        refute_change(%User{}, %{ name: @valid_model.name })
+        refute_change(%User{}, %{ name: @valid_model.name }, :registration_changeset)
     end
 
     test "only mobile" do
-        refute_change(%User{}, %{ mobile: @valid_model.mobile })
+        refute_change(%User{}, %{ mobile: @valid_model.mobile }, :registration_changeset)
     end
 
     test "only password" do
-        refute_change(%User{}, %{ password: @valid_model.password })
+        refute_change(%User{}, %{ password: @valid_model.password }, :registration_changeset)
     end
 
     test "without email" do
-        refute_change(@valid_model, %{ email: nil })
+        refute_change(@valid_model, %{ email: nil }, :registration_changeset)
     end
 
     test "without name" do
-        refute_change(@valid_model, %{ name: nil })
+        refute_change(@valid_model, %{ name: nil }, :registration_changeset)
     end
 
     test "without mobile" do
-        refute_change(@valid_model, %{ mobile: nil })
+        refute_change(@valid_model, %{ mobile: nil }, :registration_changeset)
     end
 
     test "without password" do
-        refute_change(@valid_model, %{ password: nil })
+        refute_change(@valid_model, %{ password: nil }, :registration_changeset)
     end
 
     test "valid model" do
-        assert_change(@valid_model)
+        assert_change(@valid_model, %{}, :registration_changeset)
     end
 
     test "email formatting" do
-        refute_change(@valid_model, %{ email: "test" })
+        refute_change(@valid_model, %{ email: "test" }, :registration_changeset)
         |> assert_error_value(:email, { "should contain a local part and domain separated by '@'", [validation: :email] })
 
-        refute_change(@valid_model, %{ email: "@" })
+        refute_change(@valid_model, %{ email: "@" }, :registration_changeset)
         |> assert_error_value(:email, { "should contain a local part and domain separated by '@'", [validation: :email] })
 
-        refute_change(@valid_model, %{ email: "test@" })
+        refute_change(@valid_model, %{ email: "test@" }, :registration_changeset)
         |> assert_error_value(:email, { "should contain a local part and domain separated by '@'", [validation: :email] })
     end
 
     test "mobile formatting" do
-        refute_change(@valid_model, %{ mobile: "123" })
+        refute_change(@valid_model, %{ mobile: "123" }, :registration_changeset)
         |> assert_error_value(:mobile, { "should begin with a country prefix", [validation: :phone_number] })
 
-        refute_change(@valid_model, %{ mobile: "+123a" })
+        refute_change(@valid_model, %{ mobile: "+123a" }, :registration_changeset)
         |> assert_error_value(:mobile, { "should contain the country prefix followed by only digits", [validation: :phone_number] })
 
-        refute_change(@valid_model, %{ mobile: "+" })
+        refute_change(@valid_model, %{ mobile: "+" }, :registration_changeset)
         |> assert_error_value(:mobile, { "should contain between 1 and 18 digits", [validation: :phone_number] })
 
-        refute_change(@valid_model, %{ mobile: "+1234567890123456789" })
+        refute_change(@valid_model, %{ mobile: "+1234567890123456789" }, :registration_changeset)
         |> assert_error_value(:mobile, { "should contain between 1 and 18 digits", [validation: :phone_number] })
     end
 
     test "password hashing" do
-        assert_change(@valid_model)
+        assert_change(@valid_model, %{}, :registration_changeset)
         |> refute_change_field(:password_hash)
 
-        assert_change(@valid_model, %{ password: "pass" })
+        assert_change(@valid_model, %{ password: "pass" }, :registration_changeset)
         |> assert_change_field(:password_hash)
     end
 
     test "uniqueness" do
         user = Bonbon.Repo.insert!(@valid_model)
 
-        assert_change(@valid_model, %{ email: @valid_model.email })
+        assert_change(@valid_model, %{ email: @valid_model.email }, :registration_changeset)
         |> assert_insert(:error)
         |> assert_error_value(:email, { "has already been taken", [] })
 
-        assert_change(@valid_model, %{ email: @valid_model.email <> ".test" })
+        assert_change(@valid_model, %{ email: @valid_model.email <> ".test" }, :registration_changeset)
         |> assert_insert(:ok)
 
-        assert_change(@valid_model, %{ email: "test" <> @valid_model.email })
+        assert_change(@valid_model, %{ email: "test" <> @valid_model.email }, :registration_changeset)
         |> assert_insert(:ok)
     end
 
