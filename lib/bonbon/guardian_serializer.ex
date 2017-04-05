c03807f63e71a507ec3ee7087023b5df37f13030
Stefan Johnson <sj@scrimpycat.io>
2017-04-05 17:19:28 +1000

    Support business account tokens

--- lib/bonbon/guardian_serializer.ex
+++ lib/bonbon/guardian_serializer.ex
@@ -2,8 +2,10 @@ defmodule Bonbon.GuardianSerializer do
     @behaviour Guardian.Serializer
 
     def for_token(user = %Bonbon.Model.Account.User{}), do: { :ok, "User:#{user.id}" }
+    def for_token(business = %Bonbon.Model.Account.Business{}), do: { :ok, "Business:#{business.id}" }
     def for_token(_), do: { :error, "Unknown resource type" }
 
     def from_token("User:" <> id), do: { :ok, Bonbon.Repo.get(Bonbon.Model.Account.User, id) }
+    def from_token("Business:" <> id), do: { :ok, Bonbon.Repo.get(Bonbon.Model.Account.Business, id) }
     def from_token(_), do: { :error, "Unknown resource type" }
 end
