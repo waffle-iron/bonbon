27fc677d2098c62f77e150fa6b013bc4e63cbfae
Stefan Johnson <sj@scrimpycat.io>
2017-04-11 23:45:28 +1000

    Documented store model

--- web/models/store.ex
+++ web/models/store.ex
@@ -2,6 +2,51 @@ defmodule Bonbon.Model.Store do
     use Bonbon.Web, :model
     @moduledoc """
       A model representing the different stores.
+
+      ##Fields
+
+      ###:id
+      Is the unique reference to the store entry. Is an `integer`.
+
+      ###:public
+      Whether the store is publicly listed or whether it is private (for use by [this](https://trello.com/c/K2HFzzo0)). Is a `boolean`.
+
+      ###:status
+      Is the current operating status of the store. Is a `Bonbon.Type.Store.StatusEnum`.
+
+      ###:name
+      Is the name of the store. Is a `string`.
+
+      ###:phone
+      Is the contact phone number of the store. Is a `string`.
+
+      ###:address
+      Is the address the store is located at. Is a `string`.
+
+      ###:suburb
+      Is the suburb the store is located in. Is a `string`.
+
+      ###:state
+      Is the state the store is located in. Is a `string`.
+
+      ###:zip_code
+      Is the zip code for where the store is located. Is a `string`.
+
+      ###:country
+      Is the country the store is located in. Is a `string`.
+
+      ###:geo
+      Is the geospatial coordinate the store is located at. Is a `Geo.Point`.
+
+      ###:place
+      Is the place/landmark/building the store is located inside (i.e. if it is
+      inside a shopping centre's food court). Is a `string`.
+
+      ###:pickup
+      Whether the store allows for customer pickup. Is a `boolean`.
+
+      ###:reservation
+      Whether or not the store accepts reservations. Is a `boolean`.
     """
 
     schema "stores" do
@@ -23,6 +68,18 @@ defmodule Bonbon.Model.Store do
 
     @doc """
       Builds a changeset based on the `struct` and `params`.
+
+      Enforces:
+      * `status` field is required
+      * `name` field is required
+      * `phone` field is required
+      * `address` field is required
+      * `suburb` field is required
+      * `state` field is required
+      * `country` field is required
+      * `geo` field is required
+      * `pickup` field is required
+      * `reservation` field is required
     """
     def changeset(struct, params \\ %{}) do
         struct
